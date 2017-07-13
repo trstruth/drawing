782510f3269ce0e5e4ae753f88c5e14fd3c5c28e
Tristan Struthers <tristan.struthers@mail.mcgill.ca>
2017-07-13 13:15:48 -0700

    Initial Commit

---
+++ Drawing/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Drawing
+//
+//  Created by Tristan Struthers on 7/13/17.
+//  Copyright © 2017 Tristan Struthers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
